4fd3bf7f851d59803c6c6c46dfe583544d46f80e
manucitavm-web <manucitavm@gmail.com>
2026-03-26 08:45:34 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -23,7 +23,7 @@ image = Image.open('OIG7.jpg')
 
 st.image(image,width=300)
 with st.sidebar:
-    st.subheader("Traductor")
+    st.subheader("Traductor.")
     st.write("Presiona el botón, cuando escuches la señal "
                  "Habla lo que quieres traducir, luego selecciona"   
                  " la configuración de lenguaje que necesites.")
